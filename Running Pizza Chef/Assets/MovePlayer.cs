b018475dc2d7e693ad05f5b72e2948a52a638fff
fibeesoft <fibeesoft@gmail.com>
2020-09-03 14:49:37 +0200

    Player Movement, jumping, ingredient button

---
+++ Running Pizza Chef/Assets/MovePlayer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] Transform groundChecker;
+    [SerializeField] LayerMask layerGround;
+    Rigidbody2D rb;
+    Vector2 movePos;
+    float speedX = 10f;
+    Vector2 jumpForce;
+    bool canJump = false;
+    bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpForce = new Vector2(0f, 2000f);
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckIfGrounded();
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        rb.velocity = movePos;
+    }
+
+    void CheckInput()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        movePos.x = x * speedX;
+        if (Input.GetButtonDown("Jump"))
+        {
+            canJump = true;
+        }
+
+    }
+
+    void Jump()
+    {
+        if (canJump && isGrounded)
+        {
+            rb.AddForce(jumpForce, ForceMode2D.Impulse);
+            canJump = false;
+        }
+    }
+
+    void CheckIfGrounded()
+    {
+        if(Physics2D.OverlapCircle(groundChecker.position,1f, layerGround.value))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+}
---
+++ Running Pizza Chef/Assets/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] GameManager gameManager;
+    [SerializeField] Button btnIngredient;
+    void Start()
+    {
+        AssignRandomImageToTheBtnIngredient();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void AssignRandomImageToTheBtnIngredient()
+    {
+        int rand = Random.Range(0, gameManager.ingredients.Length);
+        btnIngredient.GetComponent<Image>().sprite = gameManager.ingredients[rand];
+    }
+}
