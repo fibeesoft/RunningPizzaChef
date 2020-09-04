99374ac945923892165336c480ea101bc5ab3f6e
fibeesoft <fibeesoft@gmail.com>
2020-09-04 11:28:39 +0200

    walking grounds repeat

---
+++ Running Pizza Chef/Assets/LevelManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] walkingGrounds;
+    Transform player;
+
+    Vector3 walkingGroundCurrentPos;
+    Vector3 walkingGroundNextPos;
+    float walkingGroundNextPosShiftX = 40f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        walkingGroundCurrentPos = new Vector3(0f, -4.7f);
+        walkingGrounds[0].position = walkingGroundCurrentPos;
+    }
+
+    void SwitchWalkingGrounds()
+    {
+        walkingGroundNextPos = new Vector3(walkingGroundCurrentPos.x + walkingGroundNextPosShiftX, walkingGroundCurrentPos.y, walkingGroundCurrentPos.z);
+        if(walkingGrounds[0].position == walkingGroundCurrentPos)
+        {
+            walkingGrounds[1].position = walkingGroundNextPos;
+        }
+        else if(walkingGrounds[1].position == walkingGroundCurrentPos)
+        {
+            walkingGrounds[0].position = walkingGroundNextPos;
+        }
+        walkingGroundCurrentPos = walkingGroundNextPos;
+    }
+
+    void Update()
+    {
+        if(player.position.x > walkingGroundCurrentPos.x)
+        {
+            SwitchWalkingGrounds();
+        }
+    }
+}
